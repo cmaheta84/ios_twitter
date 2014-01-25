70be2cc9d422288d2b0d672f65feaf15267ab3e7
Chandni Maheta <chandni@shoulderhair-lm.corp.yahoo.com>
2014-01-24 17:13:27 -0800

    ios_twitter

--- twitter/TweetCell.h
+++ twitter/TweetCell.h
@@ -9,5 +9,11 @@
 #import <UIKit/UIKit.h>
 
 @interface TweetCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *idLabel;
+@property (weak, nonatomic) IBOutlet UILabel *tweetsLabel;
+@property (weak, nonatomic) IBOutlet UILabel *timestampLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
++ (NSString *)reuseIdentifier;
 @end
--- twitter/TweetCell.m
+++ twitter/TweetCell.m
@@ -26,4 +26,9 @@
     // Configure the view for the selected state
 }
 
+
++ (NSString *)reuseIdentifier {
+    return @"TweetCellIdentifier";
+}
+
 @end
